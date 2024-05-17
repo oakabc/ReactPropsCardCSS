626c53089387d0dd583a47f86f4c06128787de19
Oäk Kingkaew <oakabc@gmail.com>
2024-05-17 12:55:10 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Members</h1>
+
+    <h2>Karina</h2>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/1/12/230601_Karina_%28aespa%29.jpg"
+      alt="avatar_img"
+    />
+    <p>02-711-7111</p>
+    <p>karina@aespa.com</p>
+
+    <h2>Giselle</h2>
+    <img
+      src="https://scontent.fbkk29-1.fna.fbcdn.net/v/t1.6435-9/194085701_112865444332754_7550916990412521251_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE0gksbDR-sku3T8P3ksKrNdpExz-4mYMJ2kTHP7iZgwvpphmSndWRoa15Ds6OzlPE&_nc_ohc=Z9lwUVSBTqUQ7kNvgGzrju6&_nc_ht=scontent.fbkk29-1.fna&oh=00_AYBnuywwRJbuPxYYC__t9rcx1-N7EaVwRtuBUSvDJxEzLg&oe=666E1D78"
+      alt="avatar_img"
+    />
+    <p>02-777-1111</p>
+    <p>giselle@aespa.com</p>
+
+    <h2>Winter</h2>
+    <img
+      src="https://scontent.fbkk29-9.fna.fbcdn.net/v/t39.30808-6/440347933_797001692441726_2603917157357169127_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGiJHNAK1PITdmOKTnLLJVjduWYypYMFyZ25ZjKlgwXJl9h4mN0sm8nNG56rUPkvbA&_nc_ohc=G-Qtczr0aLEQ7kNvgF1cSav&_nc_ht=scontent.fbkk29-9.fna&oh=00_AYD_tlIWJ7qDNBqlNXCaFim6o8LxQsIT0tDKX3_RnV6ZlA&oe=664C6E82"
+      alt="avatar_img"
+    />
+    <p>02-111-7777</p>
+    <p>winter@aespa.com</p>
+
+    <h2>Ningning</h2>
+    <img
+      src="https://www.nme.com/wp-content/uploads/2024/02/ningning-versace-aespa-getty.jpg"
+      alt="avatar_img"
+    />
+    <p>02-111-7777</p>
+    <p>ningning@aespa.com</p>
+  </div>,
+  document.getElementById("root")
+);
